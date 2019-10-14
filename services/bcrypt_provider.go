5b443aa6787a793ba9b3549fc5ac600e6a670d86
ANGEL\Angel <angelotebh@hotmail.com>
2019-10-14 19:39:56 +0200

    Adds bcrypt provider tests

--- services/bcrypt_provider.go
+++ services/bcrypt_provider.go
@@ -13,14 +13,17 @@ type BcryptProvider interface {
 // MyBcryptProvider is the implementation for BcryptProvider and uses the real bcrypt package
 type MyBcryptProvider struct{}
 
+// NewMyBcryptProvider returns a new MyBryptProvider
 func NewMyBcryptProvider() *MyBcryptProvider {
 	return new(MyBcryptProvider)
 }
 
+// GenerateFromPassword generates a hashed password
 func (b *MyBcryptProvider) GenerateFromPassword(password []byte, cost int) ([]byte, error) {
 	return bcrypt.GenerateFromPassword(password, cost)
 }
 
+// CompareHashAndPassword checks if the given hashed password and the password matches
 func (b *MyBcryptProvider) CompareHashAndPassword(hashedPassword, password []byte) error {
 	return bcrypt.CompareHashAndPassword(hashedPassword, password)
 }
